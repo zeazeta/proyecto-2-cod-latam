d06e05ff4332f0397c5b95ef4354d61e6dce51fb
zeazeta <42223863+zeazeta@users.noreply.github.com>
2023-07-16 03:33:37 -0600

    Codigo final para entrega

---
+++ Js/script.js
@@ -0,0 +1,5 @@
+const cvLink = document.querySelector('#cv-link');
+cvLink.addEventListener('click', function () {
+    window.location.href = 'fille:///Users/jupialee/Downloads/proyecto-1-cod-latam-asiul/proyecto-2-cod-latam/CV';
+    console.log('Descargando CV...');
+  });
